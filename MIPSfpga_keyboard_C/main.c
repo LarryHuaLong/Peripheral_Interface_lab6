b2f891f23668b6693a9087c74587267c85708b53
LarryHuaLong <709603507@qq.com>
2018-06-21 18:11:01 +0800

    UPDATA

--- MIPSfpga_keyboard_C/main.c
+++ MIPSfpga_keyboard_C/main.c
@@ -84,13 +84,13 @@ int main()
 	{
 		keycode = *READ_IO(PS2_BASE);
 		lastkeycode = *READ_IO(PS2_BASE+4);
-		//if(lastkeycode != keycode){
+		if(lastkeycode != keycode){
 			*WRITE_IO(PS2_BASE + 4) = keycode;
 			*WRITE_IO(SEG_BASE) = keycode;
 			uart_print("keycode:");
 			uart_print(my_itoa(keycode));
 			uart_print("\n\r");
-		//}
+		}
 
 		// LEDs display
 		*WRITE_IO(IO_LEDR) = count;
@@ -234,6 +234,8 @@ void _mips_handle_irq(void *ctx, int reason)
 		keycode = *READ_IO(PS2_BASE);
 		*WRITE_IO(PS2_BASE + 4) = keycode;
 		*WRITE_IO(SEG_BASE) = keycode;
+		uart_print("PS2_interrupts occurred!:");
+		uart_print(my_itoa(reason));
 		uart_print("keycode:");
 		uart_print(my_itoa(keycode));
 		uart_print("\n\r");
